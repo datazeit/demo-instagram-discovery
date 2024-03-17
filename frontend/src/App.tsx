a1cbe173ed5e92241a32fe8b92becd4353c714aa
Wilhelm Raider <wilhelm.raider@datazeit.io>
2024-03-17 10:48:35 +0100

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,24 @@
 import "@tabler/core/dist/css/tabler.css";
 import "@tabler/core/dist/js/tabler.js";
-import './index.css';
+import "./index.css";
 import React from "react";
-import {Header, RefreshButton, StrategySwitch} from "./components/Page";
-import {NearMeButton} from "./components/NearMeButton";
-import {TextSearchButton} from "./components/TextSearch";
-import {Filters, Results} from "./components/Search";
-import {SearchContext} from "./context/Search";
-import {ISearchState} from "./interface/Search";
-import {SearchContextProvider} from "./provider/Search";
-
+import { Header, RefreshButton, StrategySwitch } from "./components/Page";
+import { NearMeButton } from "./components/NearMeButton";
+import { TextSearchButton } from "./components/TextSearch";
+import { Filters, Results } from "./components/Search";
+import { SearchContext } from "./context/Search";
+import { ISearchState } from "./interface/Search";
+import { SearchContextProvider } from "./provider/Search";
 
 export default function App() {
     // Add classes to body, as required by Tabler
-    document.body.classList.add("layout-boxed")
-    document.body.classList.add("p-sm-5")
-    document.body.classList.add("p-md-5")
-    document.body.classList.add("p-lg-5")
+    document.body.classList.add("layout-boxed");
+    document.body.classList.add("p-sm-5");
+    document.body.classList.add("p-md-5");
+    document.body.classList.add("p-lg-5");
 
     // Set page title
-    document.title = "Food Discovery with Qdrant"
+    document.title = "Social Media Discovery with eyva";
 
     // Initialize search state via context provider
     const searchState: ISearchState = SearchContextProvider();
@@ -28,7 +27,7 @@ export default function App() {
         <SearchContext.Provider value={searchState}>
             <div className="page pt-lg-3 pb-lg-3 mb-lg-3 rounded-4">
                 <div className="page-wrapper">
-                    <Header/>
+                    <Header />
                 </div>
                 <div className="page-body">
                     <div className="container-xl">
@@ -37,21 +36,20 @@ export default function App() {
                         <div className="row align-items-center">
                             <div className="col">
                                 <h2 className="page-title-lg">Recommendation results</h2>
-                                <p>Select food items you like and dislike to improve your search
-                                    results.</p>
+                                <p>Select food items you like and dislike to improve your search results.</p>
                             </div>
                             <div className="col-auto ms-auto">
-                                <StrategySwitch/>
+                                <StrategySwitch />
                             </div>
                         </div>
 
-                        <Results/>
+                        <Results />
                     </div>
                 </div>
-                <TextSearchButton/>
-                <NearMeButton/>
-                <RefreshButton/>
+                <TextSearchButton />
+                {/* <NearMeButton /> */}
+                <RefreshButton />
             </div>
         </SearchContext.Provider>
-    )
-};
+    );
+}
--- frontend/src/components/Page.tsx
+++ frontend/src/components/Page.tsx
@@ -1,62 +1,67 @@
-import {
-    IconRefresh,
-} from "@tabler/icons-react";
-import React, {useContext} from "react";
-import {SearchContext} from "../context/Search";
-import {NearMeIndicator} from "./NearMeButton";
+import { IconRefresh } from "@tabler/icons-react";
+import React, { useContext } from "react";
+import { SearchContext } from "../context/Search";
+import { NearMeIndicator } from "./NearMeButton";
 
 export const Header = () => {
-    return <div className="page-header">
-        <div className="container-xl">
-            <div className="row align-items-center">
-                <div className="col d-flex">
-                        <a href="https://qdrant.tech" target="_blank">
-                            <img src="/powered_by_qdrant.svg" width={130} alt={'Powered by Qdrant'}/>
+    return (
+        <div className="page-header">
+            <div className="container-xl">
+                <div className="row align-items-center">
+                    <div className="col d-flex">
+                        <a href="https://eyva.ai" target="_blank">
+                            <img
+                                src="/eyva_logo_white.svg"
+                                width={130}
+                                alt={"Powered by eyva.ai"}
+                                style={{ filter: "invert(100%)" }}
+                            />
                         </a>
-                    <div className="d-flex flex-column" style={{marginLeft: '1rem', marginTop: '-3px'}}>
-                        <div className="page-pretitle">
-                            Semantic search
+                        <div className="d-flex flex-column" style={{ marginLeft: "1rem", marginTop: "-3px" }}>
+                            <div className="page-pretitle">Semantic search</div>
+                            <h2 className="page-title">Social Media Discovery</h2>
                         </div>
-                        <h2 className="page-title">
-                            Food Discovery
-                        </h2>
                     </div>
-                </div>
-                <div className={"col-auto ms-auto"}>
-                    <NearMeIndicator/>
+                    <div className={"col-auto ms-auto"}>
+                        <NearMeIndicator />
+                    </div>
                 </div>
             </div>
         </div>
-    </div>;
+    );
 };
 
 export const RefreshButton = () => {
-    const {setLocation, clearFilters, retrieveResults} = useContext(SearchContext);
+    const { setLocation, clearFilters, retrieveResults } = useContext(SearchContext);
 
     const handleRefresh = () => {
         setLocation(null);
         retrieveResults(clearFilters(), null);
-    }
+    };
 
-    return <div className="ribbon ribbon-top bg-teal cursor-pointer" onClick={handleRefresh} title="Reset all">
-        <button className="switch-icon" style={{"fontSize": "2em"}}>
-            <span className="switch-icon-a text-white">
-                 <IconRefresh/>
-            </span>
-        </button>
-    </div>
+    return (
+        <div className="cursor-pointer ribbon ribbon-top bg-teal" onClick={handleRefresh} title="Reset all">
+            <button className="switch-icon" style={{ fontSize: "2em" }}>
+                <span className="text-white switch-icon-a">
+                    <IconRefresh />
+                </span>
+            </button>
+        </div>
+    );
 };
 
 export const StrategySwitch = () => {
-    const {newStrategy, setNewStrategy, retrieveResults, filters, location} = useContext(SearchContext);
+    const { newStrategy, setNewStrategy, retrieveResults, filters, location } = useContext(SearchContext);
 
     const handleSwitch = () => {
         setNewStrategy(!newStrategy);
         retrieveResults(filters, location, !newStrategy);
-    }
+    };
 
-    return <label className="form-check form-switch">
-        <input className="form-check-input" type="checkbox" onChange={handleSwitch} checked={newStrategy} />
-        <span className="form-check-label">New recommendations algorithm</span>
-    </label>
-}
+    return (
+        <label className="form-check form-switch">
+            <input className="form-check-input" type="checkbox" onChange={handleSwitch} checked={newStrategy} />
+            <span className="form-check-label">New recommendations algorithm</span>
+        </label>
+    );
+};
